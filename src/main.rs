5fcab26a3cc6b2af5437c1f45ea463eafdb3e1b5
overoxidize <overoxidize@protonmail.com>
2023-01-14 12:03:30 -0500

    Working on cli help messages

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,39 @@
-use std::{env, net};
+use std::{env, net, str};
 use net::IpAddr;
+use str::FromStr;
 
 struct Arguments {
     flag: String,
     ipaddr: IpAddr,
     threads: u16,
+}
+
+impl Arguments {
+    fn new(args: &[String]) -> Result<Arguments, &'static str> {
+
+        if args.len() < 2 {
+            return Err("not enough arguments");
+        } else if args.len() > 4 {
+            return Err("too many arguments");
+        }
+
+        let f = args[1].clone();
+
+        if let Ok(ipaddr) = IpAddr::from_str(&f) {
+            return Ok(Arguments {flag: String::from(""), ipaddr, threads})
+        }
+
+        else {
+            let flag = args[1].clone();
+
+            if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
+                println!("Usage: -j to select how many threads you want \r\n -h or -help to show this help message");
+                return Err("help");
+            }
+        }
+
+
+    }
 }
 
 fn main() {
@@ -14,10 +43,5 @@ fn main() {
 
         // A typical pattern is to call iter() on a collection, transform the contents, and then call collect at the end to group them into a collection again.
 
-    for i in &args {
-        println!("{}", i);
-    }
-
-
-    println!("{:?}", args);
+    let program = args[0].clone();
 }
