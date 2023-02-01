39729d00f91c2d151c79c36ba4d3f56f382dca15
overoxidize <overoxidize@protonmail.com>
2023-02-01 17:29:03 -0500

    finished multi-threading portion and planning extension

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use std::{env, net, str};
-use net::IpAddr;
+use std::{env, net, str, num};
+use net::{IpAddr, TcpStream};
 use std::str::FromStr;
 use std::process;
-
+use std::thread;
 use std::sync::mpsc::{Sender, channel};
-
+use std::io::{self, Write};
+const MAX: u16 = 65535;
 struct Arguments {
     flag: String,
     ipaddr: IpAddr,
@@ -31,41 +32,72 @@ impl Arguments {
         }
 
         else {
+
             let flag = args[1].clone();
 
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
                 println!("Usage: -j to select how many threads you want \r\n -h or -help to show this help message");
 
                 return Err("help");
+
             } else if flag.contains("-h") || flag.contains("-help") {
+
                 return Err("too many arguments");
+
             } else if flag.contains("-j") {
+
                 let ipaddr = match IpAddr::from_str(&args[3]) {
                     Ok(s) => s,
-                    Err(_) => Err("not a valid IPADDR; must be IPv4 or IPv6")
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
                 };
                 let threads = match args[2].parse::<u16>() {
+
                     Ok(s) => s,
                     Err(_) => return Err("failed to parse thread number")
                 };
-                return Ok(arguments {
+
+                return Ok(Arguments {
+
                     threads,
                     flag,
                     ipaddr,
                 });
-                }
-            else {
+                } else {
+
                 return Err("invalid syntax");
             }
             }
         }
 
+        
+        
+    }
+    
+    fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
+        
+        let mut port: u16 = start_port + 1;
 
-}
+        loop {
+            match TcpStream::connect((addr, port)) {
+                Ok(_) => { 
+                    print!(".");
+                    io::stdout().flush().unwrap();
+                    tx.send(port).unwrap();
+            }
+            Err(_) => {}
+            }
 
+            if (MAX - port) <= num_threads {
+                break;
+            }
+
+            port += num_threads;
+        }
+    }
 fn main() {
 
     let args: Vec<String> = env::args().collect();
+    
         // collect() can consume any iterable, and in its most basic form, will turn one collection into another.
 
         // A typical pattern is to call iter() on a collection, transform the contents, and then call collect at the end to group them into a collection again.
@@ -76,7 +108,33 @@ fn main() {
             process::exit(0);
         } else {
             eprintln!("{} problem parsing arguments: {}", program, err);
-            process::exit(o);
+            process::exit(0)
         }
     });
+
+
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+    let (tx, rx,) = channel();
+
+    for i in 0..num_threads {
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx, i, addr, num_threads);
+        });
+    }
+
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out {
+        println!("{} is open", v);
+    }
 }
